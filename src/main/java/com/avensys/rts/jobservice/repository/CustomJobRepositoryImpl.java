26f161f32f43fd2a9c603465a7a6ed2fa250874c
hxg-avensys <H.Xiang@aven-sys.com>
2024-02-28 19:17:21 +0800

    Cleaned

--- src/main/java/com/avensys/rts/jobservice/repository/CustomJobRepositoryImpl.java
+++ src/main/java/com/avensys/rts/jobservice/repository/CustomJobRepositoryImpl.java
@@ -452,7 +452,7 @@ public class CustomJobRepositoryImpl implements CustomJobRepository {
 
 		// User ID condition
 		String userCondition = "";
-		if (!userIds.isEmpty() && !jobType.equals("fod")) {
+		if (!userIds.isEmpty() && !jobType.equals("fod") && !jobType.equals("active_jobs")) {
 			userCondition = " AND created_by IN (:userIds)";
 		}
 
@@ -630,7 +630,7 @@ public class CustomJobRepositoryImpl implements CustomJobRepository {
 
 		// User ID condition
 		String userCondition = "";
-		if (!userIds.isEmpty() && !jobType.equals("fod")) {
+		if (!userIds.isEmpty() && !jobType.equals("fod") && !jobType.equals("active_jobs")) {
 			userCondition = " AND created_by IN (:userIds)";
 		}
 
@@ -729,7 +729,7 @@ public class CustomJobRepositoryImpl implements CustomJobRepository {
 
 		// User ID condition
 		String userCondition = "";
-		if (!userIds.isEmpty()) {
+		if (!userIds.isEmpty() && !jobType.equals("fod") && !jobType.equals("active_jobs")) {
 			userCondition = " AND created_by IN (:userIds)";
 		}
 
@@ -803,71 +803,6 @@ public class CustomJobRepositoryImpl implements CustomJobRepository {
 		return active;
 	}
 
-//	private String getQuery(String queryString, String jobType, Long userId, Boolean isActive, List<Long> userIds) {
-//		if (jobType != null && jobType.length() > 0) {
-//			switch (jobType) {
-//			case "new_job": {
-//				queryString = queryString.replace("{1}",
-//						"id not in (select distinct(fod.job_id) from job_recruiter_fod fod) AND ");
-//				break;
-//			}
-////			case "active_jobs": {
-////				isActive = true;
-////				queryString = queryString.replace("{1}",
-////						"id in (select distinct(fod.job_id) from job_recruiter_fod fod) AND ");
-////				break;
-////			}
-//			case "active_jobs": {
-//				queryString = queryString.replace("{1}",
-//						"id in (select distinct(job_id) from job_recruiter_fod where (recruiter_id in (:userIds) "
-//								+ "or sales_id in (:userIds))) AND ");
-//				break;
-//			}
-//			case "inactive_jobs": {
-//				isActive = false;
-//				queryString = queryString.replace("{1}",
-//						"id in (select distinct(fod.job_id) from job_recruiter_fod fod) AND ");
-//				break;
-//			}
-//			case "closed_jobs": {
-//				queryString = queryString.replace("{1}",
-//						"id in (select distinct(fod.job_id) from job_recruiter_fod fod where fod.status = 'CLOSED') AND ");
-//				break;
-//			}
-////			case "fod": {
-////				queryString = queryString.replace("{1}",
-////						"id in (select distinct(job_id) from job_recruiter_fod where recruiter_id = " + userId
-////								+ " or sales_id = " + userId + " and created_at >= current_date) AND ");
-////				break;
-////			}
-//			case "fod": {
-//				queryString = queryString.replace("{1}",
-//						"id in (select distinct(job_id) from job_recruiter_fod where (recruiter_id in (:userIds) "
-//								+ "or sales_id in (:userIds)) and created_at >= current_date) AND ");
-//				break;
-//			}
-////			case "assigned_jobs": {
-////				queryString = queryString.replace("{1}",
-////						"id in (select distinct(job_id) from job_recruiter_fod where status != 'CLOSED' and (recruiter_id = "
-////								+ userId + " or sales_id = " + userId + ")) AND ");
-////				break;
-////			}
-//			case "assigned_jobs": {
-//				queryString = queryString.replace("{1}",
-//						"id in (select distinct(job_id) from job_recruiter_fod where status != 'CLOSED' and (recruiter_id in (:userIds) "
-//								+ "or sales_id in (:userIds))) AND ");
-//				break;
-//			}
-//			default:
-//				queryString = queryString.replace("{1}", "");
-//				break;
-//			}
-//		} else {
-//			queryString = queryString.replace("{1}", "");
-//		}
-//		return queryString;
-//	}
-
 	private String getQuery(String queryString, String jobType, Long userId, Boolean isActive, List<Long> userIds) {
 		if (jobType != null && jobType.length() > 0 && !userIds.isEmpty()) {
 			switch (jobType) {
@@ -876,12 +811,6 @@ public class CustomJobRepositoryImpl implements CustomJobRepository {
 						"id not in (select distinct(fod.job_id) from job_recruiter_fod fod) AND ");
 				break;
 			}
-			// case "active_jobs": {
-			// isActive = true;
-			// queryString = queryString.replace("{1}",
-			// "id in (select distinct(fod.job_id) from job_recruiter_fod fod) AND ");
-			// break;
-			// }
 			case "active_jobs": {
 				queryString = queryString.replace("{1}",
 						"id in (select distinct(job_id) from job_recruiter_fod where (recruiter_id in (:userIds) "
@@ -899,26 +828,12 @@ public class CustomJobRepositoryImpl implements CustomJobRepository {
 						"id in (select distinct(fod.job_id) from job_recruiter_fod fod where fod.status = 'CLOSED') AND ");
 				break;
 			}
-			// case "fod": {
-			// queryString = queryString.replace("{1}",
-			// "id in (select distinct(job_id) from job_recruiter_fod where recruiter_id = "
-			// + userId
-			// + " or sales_id = " + userId + " and created_at >= current_date) AND ");
-			// break;
-			// }
 			case "fod": {
 				queryString = queryString.replace("{1}",
 						"id in (select distinct(job_id) from job_recruiter_fod where (recruiter_id in (:userIds) "
 								+ "or sales_id in (:userIds)) and created_at >= current_date) AND ");
 				break;
 			}
-			// case "assigned_jobs": {
-			// queryString = queryString.replace("{1}",
-			// "id in (select distinct(job_id) from job_recruiter_fod where status !=
-			// 'CLOSED' and (recruiter_id = "
-			// + userId + " or sales_id = " + userId + ")) AND ");
-			// break;
-			// }
 			case "assigned_jobs": {
 				queryString = queryString.replace("{1}",
 						"id in (select distinct(job_id) from job_recruiter_fod where status != 'CLOSED' and (recruiter_id in (:userIds) "
@@ -957,13 +872,6 @@ public class CustomJobRepositoryImpl implements CustomJobRepository {
 						"id in (select distinct(job_id) from job_recruiter_fod where created_at >= current_date) AND ");
 				break;
 			}
-			// case "assigned_jobs": {
-			// queryString = queryString.replace("{1}",
-			// "id in (select distinct(job_id) from job_recruiter_fod where status !=
-			// 'CLOSED' and (recruiter_id = "
-			// + userId + " or sales_id = " + userId + ")) AND ");
-			// break;
-			// }
 			case "assigned_jobs": {
 				queryString = queryString.replace("{1}",
 						"id in (select distinct(job_id) from job_recruiter_fod where status != 'CLOSED' and (recruiter_id in (:userIds) "
