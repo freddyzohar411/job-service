5127c39c236128dd28c3043212a0c0b083735b69
hxg-avensys <H.Xiang@aven-sys.com>
2024-05-08 09:32:59 +0800

    added create tagJob with attachment endpoint

--- src/main/java/com/avensys/rts/jobservice/interceptor/JwtTokenInterceptor.java
+++ src/main/java/com/avensys/rts/jobservice/interceptor/JwtTokenInterceptor.java
@@ -2,6 +2,7 @@ package com.avensys.rts.jobservice.interceptor;
 
 import com.avensys.rts.jobservice.util.JwtUtil;
 
+import com.avensys.rts.jobservice.util.RequestCtxUtil;
 import feign.RequestInterceptor;
 import feign.RequestTemplate;
 
@@ -10,5 +11,9 @@ public class JwtTokenInterceptor implements RequestInterceptor {
 	@Override
 	public void apply(RequestTemplate requestTemplate) {
 		requestTemplate.header("Authorization", "Bearer " + JwtUtil.getTokenFromContext());
+		String audit = RequestCtxUtil.getHeaderAttributes("Audit");
+		if (audit != null) {
+			requestTemplate.header("Audit", audit);
+		}
 	}
 }
