d193642760847baf0a3a9a17324b3e33b2d302d4
hxg-avensys <H.Xiang@aven-sys.com>
2024-06-26 15:26:31 +0800

    Updated some counts

---
+++ src/main/java/com/avensys/rts/jobservice/controller/JobReportController.java
@@ -0,0 +1,76 @@
+package com.avensys.rts.jobservice.controller;
+
+import com.avensys.rts.jobservice.constant.MessageConstants;
+import com.avensys.rts.jobservice.exception.ServiceException;
+import com.avensys.rts.jobservice.payload.JobCandidateStageRequest;
+import com.avensys.rts.jobservice.payload.JobListingRequestDTO;
+import com.avensys.rts.jobservice.response.JobReportCountsResponseDTO;
+import com.avensys.rts.jobservice.service.JobCandidateStageService;
+import com.avensys.rts.jobservice.service.JobReportService;
+import com.avensys.rts.jobservice.util.JwtUtil;
+import com.avensys.rts.jobservice.util.ResponseUtil;
+import jakarta.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/api/jobreports")
+public class JobReportController {
+
+	private static final Logger LOG = LoggerFactory.getLogger(JobCandidateStageController.class);
+
+	@Autowired
+	private JobCandidateStageService jobCandidateStageService;
+
+	@Autowired
+	private JwtUtil jwtUtil;
+
+	@Autowired
+	private MessageSource messageSource;
+
+	@Autowired
+	private JobReportService jobReportService;
+
+	@GetMapping("/get-counts")
+	public ResponseEntity<?> create() throws ServiceException {
+		LOG.info("Get job report counts request received");
+		JobReportCountsResponseDTO jobReportCountsResponseDTO = jobReportService.getJobReportCounts();
+		return ResponseUtil.generateSuccessResponse(jobReportCountsResponseDTO, HttpStatus.CREATED,
+				messageSource.getMessage("job.tag.created", null, LocaleContextHolder.getLocale()));
+
+	}
+
+	@PostMapping("/listing")
+	public ResponseEntity<Object> getJobListing(@RequestBody JobListingRequestDTO jobListingRequestDTO,
+			@RequestHeader(name = "Authorization") String token) {
+		Long userId = jwtUtil.getUserId(token);
+		Long jobId = jobListingRequestDTO.getJobId();
+		Integer page = jobListingRequestDTO.getPage();
+		Integer pageSize = jobListingRequestDTO.getPageSize();
+		String sortBy = jobListingRequestDTO.getSortBy();
+		String sortDirection = jobListingRequestDTO.getSortDirection();
+		String searchTerm = jobListingRequestDTO.getSearchTerm();
+		Integer stageType = null;
+		Boolean isAdmin = true;
+		List<String> searchFields = jobListingRequestDTO.getSearchFields();
+		if (searchTerm == null || searchTerm.isEmpty()) {
+			return ResponseUtil.generateSuccessResponse(
+					jobReportService.getJobTimelineListingPage(page, pageSize, sortBy, sortDirection, userId, jobId,
+							isAdmin, stageType),
+					HttpStatus.OK,
+					messageSource.getMessage(MessageConstants.MESSAGE_SUCCESS, null, LocaleContextHolder.getLocale()));
+		}
+		return null;
+	}
+
+
+}
---
+++ src/main/java/com/avensys/rts/jobservice/service/JobReportService.java
@@ -0,0 +1,171 @@
+package com.avensys.rts.jobservice.service;
+
+import com.avensys.rts.jobservice.apiclient.UserAPIClient;
+import com.avensys.rts.jobservice.entity.JobTimelineEntity;
+import com.avensys.rts.jobservice.exception.ServiceException;
+import com.avensys.rts.jobservice.repository.JobCandidateStageRepository;
+import com.avensys.rts.jobservice.repository.JobRecruiterFODRepository;
+import com.avensys.rts.jobservice.repository.JobTimelineRepository;
+import com.avensys.rts.jobservice.response.HttpResponse;
+import com.avensys.rts.jobservice.response.JobReportCountsResponseDTO;
+import com.avensys.rts.jobservice.response.JobTimelineResponseDTO;
+import com.avensys.rts.jobservice.response.UserResponseDTO;
+import com.avensys.rts.jobservice.util.JobCanddateStageUtil;
+import com.avensys.rts.jobservice.util.MappingUtil;
+import com.avensys.rts.jobservice.util.UserUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class JobReportService {
+
+	@Autowired
+	JobRecruiterFODRepository jobRecruiterFODRepository;
+
+	@Autowired
+	JobCandidateStageRepository jobCandidateStageRepository;
+
+	@Autowired
+	JobTimelineRepository jobTimelineRepository;
+
+	@Autowired
+	private UserUtil userUtil;
+
+	@Autowired
+	private UserAPIClient userAPIClient;
+
+	@Autowired
+	private MessageSource messageSource;
+
+	public JobReportCountsResponseDTO getJobReportCounts() throws ServiceException {
+		Boolean getAll = true;
+		Integer newJobsCount = getNewJobsCount(getAll);
+		Integer activeJobsCount = getActiveJobsCount(getAll);
+		// Associated
+		Long associatedCount = jobCandidateStageRepository.findActiveJobsByStageName(JobCanddateStageUtil.ASSOCIATE);
+		// Submit to Sales
+		Long submitToSalesCount = jobCandidateStageRepository
+				.findActiveJobsByStageName(JobCanddateStageUtil.SUBMIT_TO_SALES);
+		// Submit to Client
+		Long submitToClientCount = jobCandidateStageRepository
+				.findActiveJobsByStageName(JobCanddateStageUtil.SUBMIT_TO_CLIENT);
+		// Find Selected count
+		Long selectedCount = jobCandidateStageRepository.findActiveJobsByStageNameAndStatus(
+				JobCanddateStageUtil.CONDITIONAL_OFFER_ACCEPTED_OR_DECLINED, JobCanddateStageUtil.COMPLETED);
+
+		// Find Rejected count
+		Long rejectedCount = jobCandidateStageRepository.findActiveJobByStatus(JobCanddateStageUtil.REJECTED);
+
+		JobReportCountsResponseDTO jobReportCountsResponseDTO = new JobReportCountsResponseDTO();
+		jobReportCountsResponseDTO.setNewRequirementsCount(newJobsCount.longValue());
+		jobReportCountsResponseDTO.setActiveRequirementsCount(activeJobsCount.longValue());
+		jobReportCountsResponseDTO.setAssociatedCount(associatedCount);
+		jobReportCountsResponseDTO.setSubmitToSalesCount(submitToSalesCount);
+		jobReportCountsResponseDTO.setSubmitToClientCount(submitToClientCount);
+		jobReportCountsResponseDTO.setSelectedCount(selectedCount);
+		jobReportCountsResponseDTO.setRejectedCount(rejectedCount);
+		return jobReportCountsResponseDTO;
+	}
+
+	public JobTimelineResponseDTO getJobTimelineListingPage(Integer page, Integer size, String sortBy,
+			String sortDirection, Long userId, Long jobId, Boolean isAdmin, Integer stageType) {
+		// Get sort direction
+		Sort.Direction direction = Sort.DEFAULT_DIRECTION;
+		if (sortDirection != null && !sortDirection.isEmpty()) {
+			direction = sortDirection.equals("desc") ? Sort.Direction.DESC : Sort.Direction.ASC;
+		}
+		if (sortBy == null || sortBy.isEmpty() || sortBy.equals("")) {
+			sortBy = "updated_at";
+			direction = Sort.Direction.DESC;
+		}
+
+		// User condition
+		List<Long> userIds = new ArrayList<>();
+		PageRequest pageRequest = PageRequest.of(page, size, Sort.by(direction, sortBy));
+
+		Page<JobTimelineEntity> jobEntitiesPage = null;
+		// Try with numeric first else try with string (jsonb)
+		try {
+			jobEntitiesPage = jobTimelineRepository.findAllByOrderByNumericWithUserIds(userIds, false, true,
+					pageRequest, userId, jobId, stageType);
+		} catch (Exception e) {
+			e.printStackTrace();
+			jobEntitiesPage = jobTimelineRepository.findAllByOrderByStringWithUserIds(userIds, false, true, pageRequest,
+					userId, jobId, stageType);
+		}
+
+		return pageJobListingToJobListingResponseDTO(jobEntitiesPage);
+	}
+
+	private JobTimelineResponseDTO pageJobListingToJobListingResponseDTO(
+			Page<JobTimelineEntity> jobTimelineEntityPage) {
+		JobTimelineResponseDTO jobListingNewResponseDTO = new JobTimelineResponseDTO();
+		jobListingNewResponseDTO.setTotalPages(jobTimelineEntityPage.getTotalPages());
+		jobListingNewResponseDTO.setTotalElements(jobTimelineEntityPage.getTotalElements());
+		jobListingNewResponseDTO.setPage(jobTimelineEntityPage.getNumber());
+		jobListingNewResponseDTO.setPageSize(jobTimelineEntityPage.getSize());
+		List<JobTimelineEntity> list = jobTimelineEntityPage.getContent().stream().map(jobEntity -> {
+
+			// Get created by User data from user service
+			// Cast to Integer
+			HttpResponse userResponse = userAPIClient.getUserById(jobEntity.getCreatedBy().intValue());
+			UserResponseDTO userData = MappingUtil.mapClientBodyToClass(userResponse.getData(), UserResponseDTO.class);
+			jobEntity.setCreatedByName(userData.getFirstName() + " " + userData.getLastName());
+
+			// Get updated by User data from user service
+			HttpResponse updatedByUserResponse = userAPIClient.getUserById(jobEntity.getUpdatedBy().intValue());
+			UserResponseDTO updatedByUserData = MappingUtil.mapClientBodyToClass(updatedByUserResponse.getData(),
+					UserResponseDTO.class);
+			jobEntity.setUpdatedByName(updatedByUserData.getFirstName() + " " + updatedByUserData.getLastName());
+			return jobEntity;
+		}).toList();
+		jobListingNewResponseDTO.setJobs(list);
+		return jobListingNewResponseDTO;
+	}
+
+	private Integer getNewJobsCount(Boolean getAll) throws ServiceException {
+		try {
+			List<Long> userIds = new ArrayList<>();
+			Optional<Integer> entityOptional = Optional.empty();
+			if (getAll) {
+				entityOptional = jobRecruiterFODRepository.getNewJobsCountAll();
+			} else {
+				entityOptional = jobRecruiterFODRepository.getNewJobsCount(userIds);
+			}
+			if (entityOptional.isPresent()) {
+				return entityOptional.get();
+			} else {
+				return 0;
+			}
+		} catch (Exception e) {
+			throw new ServiceException(e.getLocalizedMessage());
+		}
+	}
+
+	private Integer getActiveJobsCount(Boolean getAll) throws ServiceException {
+		try {
+			List<Long> userIds = new ArrayList<>();
+			Optional<Integer> entityOptional = Optional.empty();
+			if (getAll) {
+				entityOptional = jobRecruiterFODRepository.getActiveJobsCountAll();
+			} else {
+				entityOptional = jobRecruiterFODRepository.getActiveJobsCount(userIds);
+			}
+			if (entityOptional.isPresent()) {
+				return entityOptional.get();
+			} else {
+				return 0;
+			}
+		} catch (Exception e) {
+			throw new ServiceException(e.getLocalizedMessage());
+		}
+	}
+}
